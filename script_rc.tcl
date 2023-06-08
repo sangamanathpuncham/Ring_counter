6c30ac55b06f47c966cc99509007d5f7fc1dafe4
Sangamanath-VSD_HDP <132802184+sangamanathpuncham@users.noreply.github.com>
2023-06-08 10:41:18 +0530

    Delete script_rc.tcl

--- script_rc.tcl
+++
@@ -1,5 +0,0 @@
-read_liberty /mnt/d/vlsi/sky130RTLDesignAndSynthesisWorkshop/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-read_verilog /mnt/d/vlsi/ringc/ring_synth_net1.v
-link_design ring_counter
-read_sdc rc.sdc
-report_checks -fields {nets cap slew input_pins} -digits {4} > counter_report.rpt
